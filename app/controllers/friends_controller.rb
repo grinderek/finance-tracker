163bfec86789f10a6ae35799e2e9b8ecaa4059a2
Danila Berazhny <dannmmaster@gmail.com>
2021-11-04 15:20:00 +0300

    Add errors for friends form

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -7,6 +7,16 @@ class FriendsController < ApplicationController
         respond_to do |format|
           format.js { render partial: 'users/result_friends'}
         end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "There is not such user"
+          format.js { render partial: 'users/result_friends'}
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Enter name or email to search"
+        format.js { render partial: 'users/result_friends'}
       end
     end
   end
